ff9bb3932c7bbe9c770fa56c84e219ee3500231a
Fabián E García <garciafabianenrique79@gmail.com>
2026-02-03 19:13:24 -0300

    Movimiento del jugador
    Script de movimiento

--- Assets/Scripts/Player-King/MovimientoJugador.cs
+++ Assets/Scripts/Player-King/MovimientoJugador.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
@@ -6,7 +7,7 @@ public class MovimientoJugador : MonoBehaviour
     [Header("Movimiento")]
     [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private float velocidadMovimiento;
-    [SerializeField] private float entradaHorizontal;
+    private float entradaHorizontal;
 
     private void Update()
     {
@@ -15,6 +16,30 @@ public class MovimientoJugador : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb2D.linearVelocity = new Vector2(entradaHorizontal, rb2D.linearVelocity.y);
+        ControlarMovimientoHorizontal();
+    }
+
+    private void ControlarMovimientoHorizontal()
+    {
+        rb2D.linearVelocity = new Vector2(entradaHorizontal * velocidadMovimiento, rb2D.linearVelocity.y);
+
+        if ((entradaHorizontal > 0 && !MirandoALaDerecha()) || (entradaHorizontal < 0 && MirandoALaDerecha()))
+        {
+            Girar(); //utiliza la rotación Y en 180° ya que por flip queda mal el collider
+            //porque los sprites están mal cortados o no tienen bien el centro del pivot
+
+        }
+    }
+
+    private void Girar()
+    {
+       Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+
+    private bool MirandoALaDerecha()
+    {
+       return transform.localScale.x == 1;
     }
 }
